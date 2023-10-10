9302f4fc9f8167bcbb2f408922a4b9ccf5f31101
Osvaldo Marte <o.j.marte@gmail.com>
2023-10-10 11:54:41 -0400

    fix the simulation to show the initial value and the final value after 1000 iteration

--- src/simulation.ts
+++ src/simulation.ts
@@ -3,10 +3,17 @@ import { Citizen } from "./population";
 const population: Citizen[] = Array(100).fill(null).map((_, idx) => new Citizen(`Citizen${idx + 1}`));
 let pricePerHour = 10;
 
-for (let i = 0; i < 100; i++) {
+// Print initial values of each citizen
+console.log("Initial status of each citizen:");
+population.forEach(citizen => {
+  console.log(`${citizen.name}: $${citizen.money}`);
+});
+console.log('------------------------------------');
+
+for (let i = 0; i < 1000; i++) {
   const shuffledPopulation = [...population].sort(() => 0.5 - Math.random());
 
-  const [timeConsumers, timeSellers] = [shuffledPopulation.slice(0, 50), shuffledPopulation.slice(50)];
+  const [timeConsumers, timeSellers] = [shuffledPopulation.slice(0, 500), shuffledPopulation.slice(500)];
 
   let successfulTransactions = 0;
   timeConsumers.forEach(consumer => {
@@ -26,19 +33,10 @@ for (let i = 0; i < 100; i++) {
   } else {
     pricePerHour = Math.max(1, pricePerHour - 1);
   }
-
-  const totalMoney = population.reduce((sum, p) => sum + p.money, 0);
-  const totalHoursLeft = population.reduce((sum, p) => sum + p.timeAvailable, 0);
-
-  console.log(`Round ${i + 1}:`);
-  console.log(`Price per Hour: $${pricePerHour}`);
-  console.log(`Total Money: $${totalMoney}`);
-  console.log(`Total Hours Left: ${totalHoursLeft}`);
-  console.log('------------------------------------');
 }
 
-// Finally, print out the final status of each citizen to identify who got richer or poorer
-console.log("Final status of each citizen:");
+// Print final values of each citizen after 1000 iterations
+console.log("Final status of each citizen after 1000 iterations:");
 population.forEach(citizen => {
   console.log(`${citizen.name}: $${citizen.money}`);
 });
